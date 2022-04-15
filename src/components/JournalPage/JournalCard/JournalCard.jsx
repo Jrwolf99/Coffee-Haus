c6006dd4caec62a747a252804ba0fef3a396e986
jrwolf99 <jrwolf99@outlook.com>
2022-04-14 22:52:11 -0500

    made journal local storage

---
+++ src/components/JournalPage/JournalCard/JournalCard.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import styled from "styled-components"
+
+//hooks
+import { useLocalStorage } from "../../../hooks/useLocalStorage"
+
+
+const StyledJournalCard = styled.div`
+  padding: 1em;
+  margin: .5rem;
+  background-color: white;
+  box-shadow: 0px 6px 6px rgba(0, 0, 0, 0.3);
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+    & > p {
+    width: 100%;
+    padding-left: 1em;
+    margin: 0.5rem 0;
+    }
+
+    & > textarea {
+    resize: none;
+    width: 100%;
+    height: 100%;
+    padding: 1.2em;
+    border: none;
+    outline: none;
+}
+
+`;
+
+
+
+
+export default function JournalCard({ title, placeholder, keyName }) {
+
+    const [text, setText, getText] = useLocalStorage(keyName, "");
+
+    return (
+        <StyledJournalCard>
+            <p>{title}</p>
+            <textarea
+                placeholder={placeholder}
+                onChange={(e) => setText(e.target.value)}
+                value={text}
+            ></textarea>
+        </StyledJournalCard>
+    )
+}
--- src/components/JournalPage/JournalPage.jsx
+++ src/components/JournalPage/JournalPage.jsx
@@ -1,20 +1,48 @@
 import React from 'react'
 import styled from "styled-components"
+import JournalCard from './JournalCard/JournalCard';
+
 
 
 
 const StyledJournalPage = styled.div`
     width: 73vw;
     height: 85vh;
+    overflow: auto;
     position: absolute;
-    background-color: aliceblue;
     top: 11vh;
     left: 23vw;
+    display: grid;
+    grid-template-rows: 1fr 1fr;
+    grid-template-columns: 1fr 1fr;
+
 `;
 
 
 export default function JournalPage() {
+
     return (
-        <StyledJournalPage>JournalPage</StyledJournalPage>
+        <StyledJournalPage>
+            <JournalCard
+                placeholder="What did you do today?"
+                title={"Daily Description"}
+                keyName="desc"
+            />
+            <JournalCard
+                placeholder="What is something that you learned today?"
+                title={"Today’s Lessons"}
+                keyName="lesson"
+            />
+            <JournalCard
+                placeholder="What do you want to do tomorrow?"
+                title={"Plan for Tomorrow"}
+                keyName="plan"
+            />
+            <JournalCard
+                placeholder="quote here"
+                title={"Quote of the Day"}
+                keyName="quote"
+            />
+        </StyledJournalPage>
     )
 }
